c280c3488eb8a03c49921eef376757bce7b65183
Blinov Evgeny <sharppoint@yandex.ru>
2022-10-05 22:43:04 +0300

    все модальные окна

--- src/js/index.js
+++ src/js/index.js
@@ -107,16 +107,64 @@ function sizeReaction() {
 document.addEventListener("DOMContentLoaded", sizeReaction);
 window.addEventListener("resize", sizeReaction);
 
+// ------------- MODAL BURGER -----------------------------------------//
+const burger_button = document.querySelector(".up-menu__button-burger");
+const modal_host = document.querySelector(".modal-host");
+const mobile_menu_close_button = document.querySelector(
+  ".mobile-menu__button-close"
+);
+
+function open_modal(is_open) {
+  if (is_open) {
+    modal_host.classList.add("modal-host_open");
+  } else {
+    modal_host.classList.remove("modal-host_open");
+  }
+}
+
+// открывает модальное окно по клику на кнопке burger-button
+burger_button.addEventListener("click", () => open_modal(true));
+
+// закрывает модальное окно по клику на пустом пространстве модального окна
+modal_host.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal-host")) {
+    open_modal(false);
+  }
+});
+
+// закрывает модальное окно по клику на кнопке close-button
+mobile_menu_close_button.addEventListener("click", () => open_modal(false));
+
 // ------------- MODAL FEEDBACK ---------------------------------------//
-let feedback_button = document.querySelector(".up-menu__button-chat");
+let feedback_buttons = document.querySelectorAll(".button-chat");
 let feedback_button_close = document.querySelector(".feedback-button");
 let feedback_el = document.querySelector(".feedback");
 
-let feedback_modal = new Modal(feedback_el, "feedback--open", "feedback--close");
+let feedback_modal = new Modal(feedback_el, "modal--open", "modal--close");
 
-feedback_button.addEventListener("click",()=>{  
-  feedback_modal.open();
-});
-feedback_button_close.addEventListener("click",()=>{
+feedback_buttons.forEach((bt) =>
+  bt.addEventListener("click", () => {
+    feedback_modal.open();
+    open_modal(false);
+  })
+);
+feedback_button_close.addEventListener("click", () => {
   feedback_modal.close();
+});
+
+// --------- MODAL CALL ----------------------------------------------//
+let call_buttons = document.querySelectorAll(".button-call");
+let call_button_close = document.querySelector(".call-button");
+let call_el = document.querySelector(".call");
+
+let call_modal = new Modal(call_el, "modal--open", "modal--close");
+
+call_buttons.forEach((bt) =>
+  bt.addEventListener("click", () => {
+    call_modal.open();
+    open_modal(false);
+  })
+);
+call_button_close.addEventListener("click", () => {
+  call_modal.close();
 });
